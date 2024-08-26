c13c483df17a7e914b924cbd8f3f25df87f0f3fc
NairB <brian.koeswanto@infovesta.com>
2024-08-27 05:48:43 +0700

    fix : tambah library start web spring

--- src/main/java/com/countingCharacter/controller/MenghitungCharacter.java
+++ src/main/java/com/countingCharacter/controller/MenghitungCharacter.java
@@ -1,5 +1,11 @@
 package com.countingCharacter.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
 @RestController
 public class MenghitungCharacter {
     @PostMapping("/cekinput")
