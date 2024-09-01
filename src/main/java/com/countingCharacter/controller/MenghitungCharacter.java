629ede56d7b25f11b2b8d5b6ae5f807114175e10
NairB <brian.koeswanto@infovesta.com>
2024-09-01 13:28:12 +0700

    fix : null input

--- src/main/java/com/countingCharacter/controller/MenghitungCharacter.java
+++ src/main/java/com/countingCharacter/controller/MenghitungCharacter.java
@@ -1,5 +1,6 @@
 package com.countingCharacter.controller;
 
+import jakarta.annotation.Nullable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MenghitungCharacter {
     @PostMapping("/cekinput")
-    public String hitung(@RequestBody String input) {
+    public String hitung(@RequestBody @Nullable String input) {
         int huruf = 0;
         int bilangan = 0;
         int karakterLainnya = 0;
-
+        if (input != null) {
         // Menghitung jumlah karakter, bilangan, dan huruf
         for (int i = 0; i < input.length(); i++) {
             char karakter = input.charAt(i);
@@ -29,4 +30,7 @@ public class MenghitungCharacter {
         return "Jumlah karakter: " + input.length() + "\n" + "jumlah huruf: " + huruf + "\n" + "jumlah bilangan "
                 + bilangan + "\n" + "karakterlain: " + karakterLainnya;
     }
+    return "tidak ada input";
+    }
+
 }
